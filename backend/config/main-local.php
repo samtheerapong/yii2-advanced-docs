b32f1be293f158ae0d7e74e47387c208a03e0c41
samtheerapong <theerapong.khan@gmail.com>
2023-08-11 08:01:24 +0700

    u

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -4,7 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'BackendCookie',
+            'cookieValidationKey' => 'i7T1TaGCaX_StKCBnFmPWZFk9mUzYcqu',
         ],
     ],
 ];
